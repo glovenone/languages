bd735644bb7b27041035aabc062b90b853099f0e
tian.dalong <tian.dalong@immomo.com>
2013-10-21 18:48:02 +0800

    test

--- python/website/crawler.py
+++ python/website/crawler.py
@@ -5,13 +5,27 @@ import BeautifulSoup
 
 url = "http://www.python.org/index.html"
 urls = []
-htmlSource = urllib.urlopen(url).read(200000)
-host = urllib.request(htmlSource)
+data = urllib.urlopen(url)
+htmlSource = data.read()
+#host = urllib.request(htmlSource)
 
 soup = BeautifulSoup.BeautifulSoup(htmlSource)
 for item in soup.fetch('a'):
 #    print item['href']
     urls.append(item['href'])
+#print htmlSource
+print urls
+
+print data.geturl()
+
+
 '''
-print htmlSource;
+import urllib
+google = urllib.urlopen('http://www.google.com.hk')
+print 'http header:\n', google.info()
+print 'http status:', google.getcode()
+print 'url:', google.geturl()
+#for line in google: 
+#    print line,
+google.close()
 '''
