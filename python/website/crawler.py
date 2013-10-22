977865ffc613b90e302223011ceebde4b034b581
tian.dalong <tian.dalong@immomo.com>
2013-10-22 14:37:32 +0800

    test

--- python/website/crawler.py
+++ python/website/crawler.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 #Filename:crawler.py
-import urllib
-import BeautifulSoup
 
-htmlSource = urllib.urlopen("http://sebsauvage.net/index.html").read(200000)
+"""
+import urllib2
+from bs4 import BeautifulSoup
+htmlSource = urllib.urlopen("http://http://blog.csdn.net/sding").read(200000)
 soup = BeautifulSoup.BeautifulSoup(htmlSource)
 for item in soup.fetch('a'):
     print item['href']
+
+
+"""
+import urllib2
+
+url = 'http://www.xiami.com/artist/top/id/1234'
+url = 'http://www.webguo.com/'
+content = urllib2.urlopen(url).read()
+soup = BeautifulSoup(content)
+#print content
+print soup
+"""
+"""
