edcd79da73a475a5364ff7f8b61369302c20bebe
tian.dalong <tian.dalong@immomo.com>
2013-11-11 11:01:18 +0800

    test

--- python/website/crawler.py
+++ python/website/crawler.py
@@ -12,12 +12,26 @@ for item in soup.fetch('a'):
 
 """
 import urllib2
+import re
+from bs4 import BeautifulSoup
 
 url = 'http://www.xiami.com/artist/top/id/1234'
 url = 'http://www.webguo.com/'
+#url = 'http://www.immomo.com/'
 content = urllib2.urlopen(url).read()
+content = re.findall('<a.*?href=.*?<\/a>', content, re.I)
 soup = BeautifulSoup(content)
+#url_list = soup.findAll(name='a', href=re.compile('^https?://'))
+#re.findall('<a.*?href=.*?<\/a>',ss,re.I)
+
+
 #print content
-print soup
-"""
-"""
+print type(url_list)
+print url_list
+#print "\n"
+
+for item in url_list:
+    res = item.re.compile('^https?://cn')
+    print res
+    print "\n"
+#print url_list
