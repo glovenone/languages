e1a3227b0ff4a58fb89781e4c293add2079cf7f3
tian.dalong <tian.dalong@immomo.com>
2014-01-20 18:59:01 +0800

    soup.findAll

--- python/website/test2.py
+++ python/website/test2.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 import urllib2
-from bs4 import BeautifulSoup,SoupStrainer
+from bs4 import BeautifulSoup, SoupStrainer
 import re
 import sys
 #import html5lib
 #from html5lib import sanitizer
 ##from html5lib import treebuilders
 reload(sys)
-sys.setdefaultencoding( "utf-8" )
+sys.setdefaultencoding("utf-8")
 page = urllib2.urlopen("http://www.qq.com")
 soup = BeautifulSoup(page)
 soup.originalEncoding
 soup.prettify
 
-url_list = soup.findAll(name='a',href=re.compile('^(http|https|ftp)://'))
-for each_url in url_list:
-    str_url = str(each_url).split('"')
-    print str_url[1]
+url_list = soup.findAll(name='a', href=re.compile('^(http|https|ftp)://'))
+for link in url_list:
+    print(link.get('href'))
+    #print link
+
+#url_list = soup.findAll(name='a',href=re.compile('^(http|https|ftp)://'))
+#url_list = soup.a['href']
+#print url_list
+#for each_url in url_list:
+#    str_url = str(each_url).split('"')
+#    print str_url[1]
