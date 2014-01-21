7319bc7c0c9e039d888835f1d00cfbaa7030ad49
tian.dalong <tian.dalong@immomo.com>
2014-01-21 19:52:46 +0800

    re.compile('python')

--- python/website/crawler.py
+++ python/website/crawler.py
@@ -1,37 +1,34 @@
-#!/usr/bin/python
-#Filename:crawler.py
-
-"""
-import urllib2
-from bs4 import BeautifulSoup
-htmlSource = urllib.urlopen("http://http://blog.csdn.net/sding").read(200000)
-soup = BeautifulSoup.BeautifulSoup(htmlSource)
-for item in soup.fetch('a'):
-    print item['href']
+#!/usr/bin/env python
+#coding=utf8
+import urllib2, re, sys, chardet
+from bs4 import BeautifulSoup, SoupStrainer
 
 
-"""
-import urllib2
-import re
-from bs4 import BeautifulSoup
+#import html5lib
+#from html5lib import sanitizer
+##from html5lib import treebuilders
+reload(sys)
+url = 'http://segmentfault.com/q/1010000000185278'
+html = urllib2.urlopen(url).read()
+#!/usr/bin/env python
 
-url = 'http://www.xiami.com/artist/top/id/1234'
-url = 'http://www.webguo.com/'
-#url = 'http://www.immomo.com/'
-content = urllib2.urlopen(url).read()
-content = re.findall('<a.*?href=.*?<\/a>', content, re.I)
-soup = BeautifulSoup(content)
-#url_list = soup.findAll(name='a', href=re.compile('^https?://'))
-#re.findall('<a.*?href=.*?<\/a>',ss,re.I)
+encoding = chardet.detect(html)['encoding']
+html = html.decode(encoding, 'ignore').encode('utf-8', 'ignore')
+soup = BeautifulSoup(html)
 
+#url_list = soup.findAll(name='a', href=re.compile('^(http|https|ftp)://'))
 
-#print content
-print type(url_list)
-print url_list
-#print "\n"
+a = '编码'
+b = 'python'
+url_list = soup.findAll(name='a', text=re.compile(u""+a))
+for link in url_list:
+    #print(link.get('href'))
+    print(link)
+    #print link
 
-for item in url_list:
-    res = item.re.compile('^https?://cn')
-    print res
-    print "\n"
+#url_list = soup.findAll(name='a',href=re.compile('^(http|https|ftp)://'))
+#url_list = soup.a['href']
 #print url_list
+#for each_url in url_list:
+#    str_url = str(each_url).split('"')
+#    print str_url[1]
