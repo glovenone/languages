4313d305b0c5895db0ebd837eaeeb30e79fba7ac
tian.dalong <tian.dalong@immomo.com>
2015-03-09 11:10:32 +0800

    mkdir path

--- python/website/tianyahot.py
+++ python/website/tianyahot.py
@@ -6,6 +6,7 @@ import urllib
 from bs4 import BeautifulSoup
 from datetime import * 
 import time
+import os
 
 url = 'http://bbs.tianya.cn/list.jsp?item=funinfo&grade=3&order=1' #yulebagua
 url2 = 'http://bbs.tianya.cn/list.jsp?item=free&grade=3&order=1' #tianyazatan
@@ -19,9 +20,14 @@ soup2 = str(soup2_ori).replace('<a href="/','<a href="http://bbs.tianya.cn/')
 
 timeNow = time.strftime("%Y-%m-%d--%H-%M-%S",time.localtime())
 
-path = '/Volumes/storage/weiyun_sync/tianyahot'
-f1 = path+'/tianyabagua'+str(timeNow)+'.html'
-f2 = path+'/tianyazatan'+str(timeNow)+'.html'
+weiyun_path = '/Volumes/storage/weiyun_sync/tianyahot'
+year_month = time.strftime("%Y%m",time.localtime())
+directory = weiyun_path+'/'+year_month
+if not os.path.exists(directory):
+    os.mkdir(directory)
+
+f1 = directory+'/tianyabagua'+str(timeNow)+'.html'
+f2 = directory+'/tianyazatan'+str(timeNow)+'.html'
 
 f1=open(f1,'w')
 print >>f1,soup
