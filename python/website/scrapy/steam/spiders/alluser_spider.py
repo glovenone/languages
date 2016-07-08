369e639122b921406eaf2a456b0408fe18b754ac
tiandalong <tiandalong@yunbeifenqi.com>
2016-07-08 18:42:15 +0800

    all_user

--- python/website/scrapy/steam/spiders/alluser_spider.py
+++ python/website/scrapy/steam/spiders/alluser_spider.py
@@ -1,13 +1,31 @@
 import scrapy
 
 from steam.items import AlluserItem
+import json
 
 class AlluserSpider(scrapy.Spider):
     name = "alluser"
     allowed_domains = ["alluser.org"]
+    f = file("part_user.json")
+    s = json.load(f)
+    user_ids = s.keys()
+
+    base_url = 'http://store.steampowered.com/app/'
+
+    start_urls = []
+    for i in user_ids:
+        start_urls.append(base_url + str(i) + '/')
+
+    f.close
+    print start_urls
     start_urls = [
-        "http://store.steampowered.com/app/338390/"
+        # "http://steamspy.com/api.php?request=top100in2weeks"
+        #"http://steamspy.com/api.php?request=all"
+        'http://store.steampowered.com/app/431470/',
+        'http://store.steampowered.com/app/242320/'
     ]
+
+
 
     def parse(self, response):
         print '---------------------'
@@ -19,5 +37,6 @@ class AlluserSpider(scrapy.Spider):
             item['desc'] = sel.xpath('text()').extract()
             yield item
         print '====================='
+
 
 # scrapy crawl alluser -o items.json
