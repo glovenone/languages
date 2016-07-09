3fe0fa2dfd199be60e276406e189393f2f419113
田大龙 <tiandalong@yunbeifenqi.com>
2016-07-09 23:59:13 +0800

    add header

--- python/website/scrapy/steam/spiders/alluser_spider.py
+++ python/website/scrapy/steam/spiders/alluser_spider.py
@@ -1,12 +1,13 @@
 import scrapy
 
 from steam.items import AlluserItem
+import os
 import json
 
 class AlluserSpider(scrapy.Spider):
     name = "alluser"
     allowed_domains = ["alluser.org"]
-    f = file("part_user.json")
+    f = file("all_user.json")
     s = json.load(f)
     user_ids = s.keys()
 
@@ -14,28 +15,33 @@ class AlluserSpider(scrapy.Spider):
 
     start_urls = []
     for i in user_ids:
-        start_urls.append(base_url + str(i) + '/')
-
+        start_url = base_url + str(i) + '/'
+        start_urls.append(start_url)
+        command = 'wget --header="Accept-Language:zh-CN,zh;q=0.8" -O steam_data_cn/'+str(i)+'.html '+start_url
+        os.system(command)
     f.close
-    print start_urls
+
     start_urls = [
         # "http://steamspy.com/api.php?request=top100in2weeks"
         #"http://steamspy.com/api.php?request=all"
         'http://store.steampowered.com/app/431470/',
-        'http://store.steampowered.com/app/242320/'
+        # 'http://store.steampowered.com/app/242320/'
     ]
+    os.system('')
+    exit()
 
 
 
     def parse(self, response):
         print '---------------------'
-        print response.xpath('//a')
-        for sel in response.xpath('//ul/li'):
-            item = AlluserItem()
-            item['title'] = sel.xpath('a/text()').extract()
-            item['link'] = sel.xpath('a/@href').extract()
-            item['desc'] = sel.xpath('text()').extract()
-            yield item
+        test = response.xpath('//a[@class="highlight_screenshot_link"]/img').extract()
+        print test
+        # for sel in response.xpath('//ul/li'):
+        #     item = AlluserItem()
+        #     item['title'] = sel.xpath('a/text()').extract()
+        #     item['link'] = sel.xpath('a/@href').extract()
+        #     item['desc'] = sel.xpath('text()').extract()
+        #     yield item
         print '====================='
 
 
